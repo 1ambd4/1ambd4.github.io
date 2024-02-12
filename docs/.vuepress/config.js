d0726d4d41bf9496b9680e7f552e7b29a02adda1
1ambd4 <dr.chen@mail.ustc.edu.cn>
2024-02-12 19:42:39 +0800

    docs: Hello VuePress!

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "1ambd4's blog",
+  theme: "reco",
+  description: "There is nothing new under the sun.",
+  themeConfig: {
+    author: "1ambd4",
+    type: "blog",
+    subSidebar: "auto",
+    // logo: "/avatar.png",
+    authorAvatar: "/avatar.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    nav: [
+      { text: "Home", link: "/" },
+      {
+        text: "1ambd4",
+        items: [{ text: "Github", link: "https://github.com/1ambd4" }],
+      },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroText: 紫薯布丁
+heroImage: "/avatar.png"
+heroImageStyle:
+  {
+    maxHeight: "288px",
+    display: block,
+    borderRadius: "49% 81% 33% 77% / 44% 57% 43% 56%",
+    boxShadow: "0 25px 28px rgba(0,0,0,0.2)",
+  }
+---
